e5649c1e63f999c7c1dceb4ab0cb03d614fe7653
med-salah-ben <med.salah.ben.amou@gmail.com>
2023-03-20 10:16:21 +0100

    dotnet app get & add with validation

---
+++ SlhBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlhBookWeb.Data;
+using SlhBookWeb.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
+
+namespace SlhBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDBContext _db;
+
+        public CategoryController(AppDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()    
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+
+        //Get Create Url 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //method is post we need to write [HttpPost]
+        [HttpPost]
+
+        //we need to write [ValidateAntiForgeryToken] To help and prevent the cross site request forgery attack
+        // and automatically inject a key in the form method = Post 
+        //Not Required But highly recommend that 
+        [ValidateAntiForgeryToken]
+        //Post New Category
+        public IActionResult Create(Category obj)
+        {
+            //Custom  Error 
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                //---  error just for asp - validation - summary
+                //ModelState.AddModelError("CustomError", "The Display Order cannot match the Name.");
+                //--- Error for Name & asp-validation-summary
+                ModelState.AddModelError("name", "The Display Order cannot match the Name.");
+
+            }
+
+            //Check if model is valid ? 
+            if (ModelState.IsValid) {
+
+            //when we have to add somthing to the table we Use the Add() method
+            //and we pass the argument the Add() method like this new Object
+            //We still need to push this to db
+            _db.Categories.Add(obj);
+
+            //push this Object to database
+            _db.SaveChanges();
+
+            //redirect to the index and get updated list || Data with the GET Index action 
+            return RedirectToAction("index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ SlhBookWeb/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SlhBookWeb.Models;
+
+namespace SlhBookWeb.Data
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
